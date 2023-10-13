e1a68ca4edc7e417afba2ac4c1015655f165909a
hamed <hamedahmedgoda>
2023-10-13 16:18:06 +0300

    first commit

---
+++ src/CreateOne/CreateOne.jsx
@@ -0,0 +1,7 @@
+function CreateOne() {
+    return (
+        <h1>hello hamed from IoT</h1>
+    )
+    
+}
+export default CreateOne;
---
+++ src/CreateTwo/CreateTwo.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class CreateTwo extends Component { 
+    render() {
+        return (
+            <h1>i am from marsa matrouh and i am 23  </h1>
+        )
+    }
+}
+export default CreateTwo;
